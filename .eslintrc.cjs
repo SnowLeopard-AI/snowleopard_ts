a18890f6d86dddccd10d71573bda62356694702c
Abdul Alkhatib <abdul@snowleopard.ai>
2025-12-03 11:20:59 -0800

    lint with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
