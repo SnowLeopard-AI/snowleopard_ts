f4169532cd42aa2abd1eab6ef8f3516875d958ec
Abdul Alkhatib <abdul@snowleopard.ai>
2025-12-03 13:27:56 -0800

    Turn off warnings  + update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
     es6: true,
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
